cd7c6298e35f270aa98346edd4f114d2bf76e783
Laura Schavsinski - ETPA Rennes <74655045+Laura-Schavsinski@users.noreply.github.com>
2021-10-25 17:15:29 +0200

    blop

--- Assets/Scrips/Tower.cs
+++ Assets/Scrips/Tower.cs
@@ -8,6 +8,14 @@ public class Tower : MonoBehaviour
     private GameObject upgrade { get { return _upgrade; } }
 
     private List<Enemy> enemies { get; set; } = null;
+
+    public float _fireRate = 0.1f;
+    private float fireRate { get { return _fireRate; } set { _fireRate = value; } }
+
+    private float currentFireRate { get; set; } = 0;
+
+    private bool canShoot { get; set; } = true;
+
     private void Start()
     {
         enemies = new List<Enemy>();
@@ -16,7 +24,36 @@ public class Tower : MonoBehaviour
 
     private void Update()
     {
-        
+        if (!canShoot)
+        {
+            currentFireRate += Time.deltaTime;
+            if (currentFireRate >= fireRate)
+            {
+                canShoot = true;
+                currentFireRate = 0;
+            }
+            return;
+        }
+      
+
+        if (enemies != null && enemies.Count > 0)
+        {
+           // if(canShoot)
+            {
+                //Shoot on the enemy
+                Debug.Log("Shoot");
+                canShoot = false;
+            }
+        }
+        //else if(!canShoot)
+        //{
+           // currentFireRate += Time.deltaTime;
+            //if(currentFireRate >= fireRate)
+           // {
+              //  canShoot = true;
+              //  currentFireRate = 0;
+            //}
+        //}
     }
 
     public void Upgrade()
@@ -38,6 +75,12 @@ public class Tower : MonoBehaviour
             enemies.Add(enemy);
         }
     }
-
-
+    private void OnTriggerExit(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemies.Remove(enemy);
+        }
+    }
 }
